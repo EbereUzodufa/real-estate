45eb91eebd3bc41bd11c8e8ae031e9a0166b6d33
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-08-22 02:53:27 +0100

    creates Property class with constructor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+import { type } from "os";
+
 console.log('\'Allo \'Allo!');
 
 // Uncomment to enable Bootstrap tooltips
@@ -7,3 +9,20 @@ console.log('\'Allo \'Allo!');
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+
+class Property{
+    constructor({name, images, desc, characterristics, community, price, neighborhood, mapLocation, features, isFeatured, offerType, isOnSlider}){
+        this.name = name;
+        this.images = images;
+        this.desc = desc;
+        this.characterristics = characterristics;
+        this.community = community;
+        this.price = price;
+        this.neighborhood = neighborhood;
+        this.mapLocation = mapLocation;
+        this.features = features;
+        this.isFeatured = isFeatured;
+        this.offerType = offerType;
+        this.isOnSlider = isOnSlider;
+    }
+}
